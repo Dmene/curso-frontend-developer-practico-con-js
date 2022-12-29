22a69dcb86bcdf67c42e8b5165d096357530a2c4
Didier Meneses <inmodadev3@gmail.com>
2022-12-29 11:18:40 -0500

    mobile menu responsive

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+
 
 menuEmail.addEventListener('click', toogleDesktopMenu);
+menuHamIcon.addEventListener('click', toogleMobileMenu);
+
 
 function toogleDesktopMenu (){
     console.log('cli')
     desktopMenu.classList.toggle('inactive')
+} 
+
+function toogleMobileMenu (){
+    console.log('cli')
+    mobileMenu.classList.toggle('inactive')
 } 
