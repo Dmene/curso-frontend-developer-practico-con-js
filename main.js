3f40cfe73976ebdca8d1ccac222f70b27bc99e00
Didier Meneses <inmodadev3@gmail.com>
2022-12-29 12:17:09 -0500

    carrito de compras activar y desactivar

--- main.js
+++ main.js
@@ -2,18 +2,38 @@ const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
-
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click', toogleDesktopMenu);
 menuHamIcon.addEventListener('click', toogleMobileMenu);
-
+menuCarritoIcon.addEventListener('click', toogleCarritoAside);
 
 function toogleDesktopMenu (){
-    console.log('cli')
+    const isAsideMenuClosed = aside.classList.contains('inactive');
+
+    if (!isAsideMenuClosed) {
+     aside.classList.add('inactive');
+    } 
     desktopMenu.classList.toggle('inactive')
 } 
 
 function toogleMobileMenu (){
-    console.log('cli')
-    mobileMenu.classList.toggle('inactive')
+    const isAsideMenuClosed = aside.classList.contains('inactive');
+
+    if (!isAsideMenuClosed) {
+     aside.classList.add('inactive');
+    } 
+     mobileMenu.classList.toggle('inactive');    
 } 
+
+function toogleCarritoAside(){
+  const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+
+    if (!isMobileMenuClosed) {
+      mobileMenu.classList.add('inactive');
+    } 
+    
+    aside.classList.toggle('inactive');
+
+}
